0e88b3c7cbb55fd6fdcdd2450f404aa38bad9a87
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-10-06 12:49:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -69,8 +69,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ur_rishu_143")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ur_support07")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/NenoBots")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FRIENDSCHATZONEOFFCIAL")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
